571f5e04debb088aa254ee885bb5181f9c6074d3
rickrribeiro <rickribeiro99@gmail.com>
2024-02-01 13:42:17 -0300

    removing build

--- api/build/services/enums/ai-providers-enum.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AIProvidersEnum = void 0;
-class AIProvidersEnum {
-}
-exports.AIProvidersEnum = AIProvidersEnum;
-AIProvidersEnum.CHATGPT = 'CHATGPT';
--- api/build/services/enums/social-media-enum.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SocialMediaEnum = void 0;
-class SocialMediaEnum {
-}
-exports.SocialMediaEnum = SocialMediaEnum;
-SocialMediaEnum.TINDER = 'TINDER';
